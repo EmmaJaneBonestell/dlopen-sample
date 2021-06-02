9c4760147efd4b4b32baa03829a739494f575c41
Emma Jane Bonestell <EmmaJaneBonestell@gmail.com>
2021-06-02 15:54:50 -0500

    Update bromine.c

--- bromine.c
+++ bromine.c
@@ -1,5 +1,5 @@
 #include "chemical.h"
 
-void chemical_name(const char *formula) {
-    printf("The IUPAC name is %s.\n", formula);
+void chemical_name (const char *formula) {
+    printf("The IUPAC name of %s is hypobromous acid.\n", formula);
 }
