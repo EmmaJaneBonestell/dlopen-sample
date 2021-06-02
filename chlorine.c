1cadc08563504369c159382044d2508a2a935e9f
Emma Jane Bonestell <EmmaJaneBonestell@gmail.com>
2021-06-02 15:54:00 -0500

    Update chlorine.c

--- chlorine.c
+++ chlorine.c
@@ -1,5 +1,5 @@
 #include "chemical.h"
 
-void chemical_name(const char *formula) {
-    printf("The IUPAC name is %s.\n", formula);
+void chemical_name (const char *formula) {
+    printf("The IUPAC name name of %s is hypochlorous acid.\n", formula);
 }
