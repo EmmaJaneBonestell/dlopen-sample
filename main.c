07473464f974051bfb42e4d7237b955303486cd2
Emma Jane Bonestell <EmmaJaneBonestell@gmail.com>
2021-06-02 18:10:34 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
+include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dlfcn.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char *argv[]) {
     void *handle;
     void *handle2;
     void (*formula_printf)(const char*);
-    void (*formula_printf2)(const char *);
 
     if (argc < 2 || argc > 3) {
         fprintf(stderr, "Improper number of arguments : %s .\n", argv[0]);
@@ -16,18 +15,18 @@ int main(int argc, char** argv) {
 
     if (strcmp(argv[1], "HOCl") == 0) {
         handle = dlopen("./chlorine.so", RTLD_NOW);
-        if (strcmp(argv[2], "HOBr") == 0) {
+        if (argc == 3 && strcmp(argv[2], "HOBr") == 0) {
             handle2 = dlopen("./bromine.so", RTLD_NOW);
         }
     } else if (strcmp(argv[1], "HOBr") == 0) {
         handle = dlopen("./bromine.so", RTLD_NOW);
-        if (strcmp(argv[2], "HOCl") == 0) {
+        if (argc == 3 && strcmp(argv[2], "HOCl") == 0) {
             handle2 = dlopen("./chlorine.so", RTLD_NOW);
         }
     } else {
         fprintf(stderr, "Error: unknown formula: %s\n", argv[1]);
         return EXIT_FAILURE;
-    }
+    } 
 
     if (!handle) {
         /* fail to load the library */
@@ -54,16 +53,16 @@ int main(int argc, char** argv) {
             return EXIT_FAILURE;
         }
 
-        *(void**)(&formula_printf2) = dlsym(handle2, "chemical_name");
+        *(void**)(&formula_printf) = dlsym(handle2, "chemical_name");
 
-        if (!formula_printf2) {
+        if (!formula_printf) {
             /* no such symbol */
             fprintf(stderr, "Error: %s\n", dlerror());
             dlclose(handle2);
             return EXIT_FAILURE;
         }
 
-        formula_printf2(argv[2]);
+        formula_printf(argv[2]);
         dlclose(handle2);
     }
 
