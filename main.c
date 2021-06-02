5e339dd8c7685a062b5b30dc61e07afdbe4189a9
Emma Jane Bonestell <EmmaJaneBonestell@gmail.com>
2021-06-02 16:17:42 -0500

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main(int argc, char** argv) {
     void *handle;
     void *handle2;
     void (*formula_printf)(const char*);
+    void (*formula_printf2)(const char *);
 
     if (argc < 2 || argc > 3) {
         fprintf(stderr, "Improper number of arguments : %s .\n", argv[0]);
@@ -15,22 +16,20 @@ int main(int argc, char** argv) {
 
     if (strcmp(argv[1], "HOCl") == 0) {
         handle = dlopen("./chlorine.so", RTLD_NOW);
+        if (strcmp(argv[2], "HOBr") == 0) {
+            handle2 = dlopen("./bromine.so", RTLD_NOW);
+        }
     } else if (strcmp(argv[1], "HOBr") == 0) {
         handle = dlopen("./bromine.so", RTLD_NOW);
-    } else if (strcmp(argv[1], "HOCl") == 0 
-               && strcmp(argv[2], "HOBr") == 0) {
-        handle = dlopen("./chlorine.so", RTLD_NOW);
-        handle2 = dlopen("./bromine.so", RTLD_NOW);
-    } else if (strcmp(argv[1], "HOBr") == 0 
-               && strcmp(argv[2], "HOCl") == 0) {
-        handle = dlopen("./bromine.so", RTLD_NOW);
-        handle2 = dlopen("./chlorine.so", RTLD_NOW);
+        if (strcmp(argv[2], "HOCl") == 0) {
+            handle2 = dlopen("./chlorine.so", RTLD_NOW);
+        }
     } else {
         fprintf(stderr, "Error: unknown formula: %s\n", argv[1]);
         return EXIT_FAILURE;
     }
 
-    if (!handle || (argc == 3 && !handle && !handle2)) {
+    if (!handle) {
         /* fail to load the library */
         fprintf(stderr, "Error: %s\n", dlerror());
         return EXIT_FAILURE;
@@ -45,23 +44,26 @@ int main(int argc, char** argv) {
     }
 
     formula_printf(argv[1]);
+    dlclose(handle);
 
     if (argc == 3) {
-        *(void**)(&formula_printf) = dlsym(handle2, "chemical_name");
 
-        if (!formula_printf) {
+        if (!handle2) {
+           /* fail to load the library */
+            fprintf(stderr, "Error: %s\n", dlerror());
+            return EXIT_FAILURE;
+        }
+
+        *(void**)(&formula_printf2) = dlsym(handle2, "chemical_name");
+
+        if (!formula_printf2) {
             /* no such symbol */
             fprintf(stderr, "Error: %s\n", dlerror());
             dlclose(handle2);
             return EXIT_FAILURE;
         }
 
-        formula_printf(argv[2]);
-
-    }
-
-    dlclose(handle);
-    if (argc == 3) {
+        formula_printf2(argv[2]);
         dlclose(handle2);
     }
 
